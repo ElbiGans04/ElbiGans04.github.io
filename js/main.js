bf3e84ecdf95b15cb7c4a48183f6d5c2a5732113
rhafaelbijaksana2003 <rhafaelbijaksana2003@gmail.com>
2020-07-02 12:29:44 +0700

    Fix Bug and Update version 1.0.2

--- js/main.js
+++ js/main.js
@@ -1,132 +1,151 @@
+// Pindah
+$('.goTo').click(function(e) {
+  const tujuan = $(this).attr('href');
+  const Elementtujuan = $(tujuan);
 
 
-  // Pindah
-  $('.goTo').click(function(e){
-    const tujuan = $(this).attr('href');
-    const Elementtujuan = $(tujuan);
-
-
-    $('html , body').animate({
+  $('html , body').animate({
       scrollTop: Elementtujuan.offset().top - 60
-     });
+  });
 
 
 
-    e.preventDefault();
+  e.preventDefault();
 
-  })
-  
-
-  // Scroll
+})
 
 
-    //Scroll Tampil
-    $('.animate-box').waypoint(function(direction){
-    if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+// Scroll
+$('.goTo').on('click' , function(e){
+  const tujuan = $(this).attr('href');
+  const Elementtujuan = $(tujuan);
+
+
+  $('html , body').animate({
+    scrollTop: Elementtujuan.offset().top - 60
+   });
+
+
+
+  e.preventDefault();
+});
+
+//Scroll Tampil
+$('.animate-box').waypoint(function(direction) {
+  if (direction === 'down' && !$(this.element).hasClass('animated')) {
 
 
       $(this.element).addClass('item-animate');
-      setTimeout(function(){
-        $('body .animate-box.item-animate').each(function(e){
-          var el = $(this);
-						setTimeout( function () {
-								el.addClass('fadeInUp animated');
-							  el.removeClass('item-animate');
-						},  e * 200, 'easeInOutExpo' );
-        })
-      },100)
+      setTimeout(function() {
+          $('body .animate-box.item-animate').each(function(e) {
+              var el = $(this);
+              setTimeout(function() {
+                  el.addClass('fadeInUp animated');
+                  el.removeClass('item-animate');
+              }, e * 200, 'easeInOutExpo');
+          })
+      }, 100)
 
-    }
+  }
 
-  } , {offset: '85%'} )
-
-
-    // Event saat gambar diklik
-    $('.wadahPortofolio').on('click' , '.imgPortofolio'   ,  function(e){
-      let modal = $('.modal-fael');
-      $(modal).css('display' , 'flex')
-      console.log(modal)
-      $(modal).children().children('.modal-fael-gambar').attr('src' , this.src)
-      
-      //buat animasi
-      $( modal ).animate({
-          opacity: 1,
-        }, 500 ); 
-
-      // Animasi Content
-      $(modal).children('.content').animate({
-        top: "0px"
-      },500)
-
-    })
-
-    // Event saat tombol close modal diklik
-    $('.modal-close').click(function(){
-      let modal = $('.modal-fael');
-      
-      //buat animasi
-      $(modal).animate({
-        opacity: "0"
-      },500)
-
-      //animasi content
-      $(modal).children('.content').animate({
-        top: "-100px"
-      },500)
-
-      //tunggu beberapa saat lalu ubah kembali modal menjadi none
-      setTimeout(function(){
-        $(modal).css('display' , 'none')
-      },500)
-    })
+}, {
+  offset: '85%'
+})
 
 
+// Event saat gambar diklik
+$('.wadahPortofolio').on('click', '.imgPortofolio', function(e) {
+  let modal = $('.modal-fael');
+  $(modal).css('display', 'flex')
+  console.log(modal)
+  $(modal).children().children('.modal-fael-gambar').attr('src', this.src)
+
+  //buat animasi
+  $(modal).animate({
+      opacity: 1,
+  }, 500);
+
+  // Animasi Content
+  $(modal).children('.content').animate({
+      top: "0px"
+  }, 500)
+
+})
+
+// Event saat tombol close modal diklik
+$('.modal-close').click(function() {
+  let modal = $('.modal-fael');
+
+  //buat animasi
+  $(modal).animate({
+      opacity: "0"
+  }, 500)
+
+  //animasi content
+  $(modal).children('.content').animate({
+      top: "-100px"
+  }, 500)
+
+  //tunggu beberapa saat lalu ubah kembali modal menjadi none
+  setTimeout(function() {
+      $(modal).css('display', 'none')
+  }, 500)
+})
 
 
 
-    //Event gerak
-    $('#owlPertama').owlCarousel({
-      loop: true,
-      items: 1,
-      nav: true,
-      dots: false,
-      stagePadding: 20,
-      responsiveClass: !0,
-      margin: 30,
-      autoplay: true,
-      navText: ['<i class="fas fa-arrow-left portofolio-prev portofolio-nav"></i>', '<i class="fas fa-arrow-right portofolio-next portofolio-nav"></i>']//Element nav
-      ,animateOut: 'fadeOut',
-      animateIn: 'fadeIn'
 
-    })
+//Event gerak
+$('#owlPertama').owlCarousel({
+  loop: true,
+  items: 1,
+  nav: true,
+  dots: false,
+  stagePadding: 20,
+  responsiveClass: !0,
+  margin: 30,
+  autoplay: true,
+  navText: ['<i class="fas fa-arrow-left portofolio-prev portofolio-nav"></i>', '<i class="fas fa-arrow-right portofolio-next portofolio-nav"></i>'] //Element nav
+      ,
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn'
 
-    let $featureLinks = $('.feature-link');
-    $('.owl-carousel').on("changed.owl.carousel", function (e){
-            var o = e.item.index+ 1 - e.relatedTarget._clones.length / 2,
-                n = e.item.count;
-            (o > n || 0 == o) && (o = n - o % n), o--;
-            var t = $(".feature-link:nth(" + o + ")");
-            $featureLinks.removeClass("active");
-            t.addClass("active").fadeIn('slow')
-        })
+})
 
-    $featureLinks.on("click", function () {
-            var e = $(this).data("owl-items");
-            $('.owl-carousel').trigger("to.owl.carousel", e), a($(this));
-        });
-    
+let $featureLinks = $('.feature-link');
+$('.owl-carousel').on("changed.owl.carousel", function(e) {
+  var o = e.item.index + 1 - e.relatedTarget._clones.length / 2,
+      n = e.item.count;
+  (o > n || 0 == o) && (o = n - o % n), o--;
+  var t = $(".feature-link:nth(" + o + ")");
+  $featureLinks.removeClass("active");
+  t.addClass("active").fadeIn('slow')
+})
+
+$featureLinks.on("click", function() {
+  var e = $(this).data("owl-items");
+  $('.owl-carousel').trigger("to.owl.carousel", e), a($(this));
+});
 
 
-    $('#owlKedua').owlCarousel({
-      animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: false,
-		   loop:true,
-		   margin:0,
-		   nav: false,
-		   dots: true,
-		   autoHeight: true,
-		   mouseDrag: false,
-		   autoplayHoverPause: true,
-		   items: 1
-    })
+
+$('#owlKedua').owlCarousel({
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  autoplay: true,
+  loop: true,
+  margin: 0,
+  nav: false,
+  dots: true,
+  dotsData: true,
+  autoHeight: true,
+  mouseDrag: false,
+  autoplayHoverPause: true,
+  items: 1
+})
+
+$('.owl-dot').click(function () {
+  $('.owl-dot').removeClass('active');
+  $(this).addClass('active');
+  $('.owl-carousel').trigger('to.owl.carousel', [$(this).index(), 300]);
+});
